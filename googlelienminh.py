77ab9b3da20e9672d57c43bc7b4e6838647d9205
phat <hungphat>
2020-06-20 14:49:56 +0700

    Update code get champion name

--- googlelienminh.py
+++ googlelienminh.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="/home/hungphat/Phat/SeleniumTrang/chromedriver")
+driver = webdriver.Chrome(executable_path="./chromedriver")
 driver.implicitly_wait(10)
 
-url = 'https://google.com'
+url = 'https://lienminh.garena.vn'
 
 driver.get(url)
-q = driver.find_element_by_name("q")
-q.send_keys("lien minh huyen thoai")
-q.submit()
 
-c = driver.find_element_by_xpath("//div//h3").click()
+driver.find_element_by_xpath('//a[text()="Giới thiệu"]').click()
+driver.find_element_by_xpath('//*[@id="toc"]/div[3]/div[2]/div[2]/div[4]/div[1]/a').click()
+q = "/html/body/div[1]/div/div/div[3]/div[2]/div[2]/div[2]/div[1]/div/div/div/ul/li"
+champion_list = driver.find_elements_by_xpath(q)
+
+for champion in champion_list:
+    champion_name = champion.text
+
+# driver.find_element_by_link_text("/game-info]").click()
