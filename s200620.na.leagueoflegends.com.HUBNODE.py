7be82d8588d1cf9d53a8f933f404d17f9dae6d87
namgivu <namgivu@gmail.com>
2020-06-20 18:18:11 +0700

    print found names

--- s200620.na.leagueoflegends.com.HUBNODE.py
+++ s200620.na.leagueoflegends.com.HUBNODE.py
@@ -18,8 +18,13 @@ wd.get(url)
 q = '//*[@id="gatsby-focus-wrapper"]/div/div[2]/section/div[2]/section[2]/div[2]/a'
 champion_list = wd.find_elements_by_xpath(q)
 
+champion_name_all = []
 for champion in champion_list:
     champion_name = champion.text
-    i =122
+    if champion_name:
+        champion_name_all.append(champion_name)
 
 wd.quit()
+
+print(champion_name_all)
+print(f'{len(champion_name_all)} found.')
